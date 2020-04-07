d406bce24d3c04295394c33c6ae110e8ddb4438b
bruce willis <fashun_vyctum@yahoo.com>
2020-04-07 20:57:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,5 @@
 class School
-  hash["new_key"] 
-  hash["new_key"] << "new_value_for_value_array"
   
-  school.add_student("Zach Morris", 9)
-  school.add_student("AC Slater", 9)
-  school.add_student("Kelly Kapowski", 10)
-  school.add_student("Screech", 11)
-  school.roster
 end
 
 school = School.new("Bayside High School")
