0c3db89e6c5440802699411faa34477c4ae55e37
bruce willis <fashun_vyctum@yahoo.com>
2020-04-07 20:49:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,7 +4,9 @@ class School
   hash["new_key"] << "new_value_for_value_array"
   
   school.add_student("Zach Morris", 9)
-  
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
   school.roster
 end
 
